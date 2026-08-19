1babe80ab871379862816a1c829650f1d279d6d6
GUNASHANKAR D L <gunashankardl.132371@marwadiuniversity.ac.in>
2026-08-19 19:36:47 +0530

    Add SelectionSort.py with selection sort implementation
    Implement selection sort algorithm with time and space complexity analysis.

---
+++ Practical-1/SelectionSort.py
@@ -0,0 +1,25 @@
+Selection Sort
+def selection_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        min_index = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+arr = [28,18,31,12,7,2]
+
+print("Before sorting:", arr)
+selection_sort(arr)
+print("After sorting:", arr)
+# Selection Sort
+# Time Complexity:
+# Best Case    : O(n^2)
+# Average Case : O(n^2)
+# Worst Case   : O(n^2)
+#
+# Space Complexity:
+# O(1)
