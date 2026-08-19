b649d423322ab3e58c764c028cb0d0b19ef409ff
GUNASHANKAR D L <gunashankardl.132371@marwadiuniversity.ac.in>
2026-08-19 20:40:03 +0530

    Added InteractiveSort

--- HeapSort.py
+++ HeapSort.py
@@ -1,3 +1,4 @@
+
 def heapify(arr, n, i):
     largest = i
     left = 2 * i + 1
---
+++ Interactivesort.py
@@ -0,0 +1,11 @@
+def iterative_factorial(n):
+    fact = 1
+
+    for i in range(1, n + 1):
+        fact *= i
+
+    return fact
+arr = [5, 3, 8, 4, 2]
+n = 5
+result = iterative_factorial(n)
+print(f"Factorial of {n} is: {result}")
