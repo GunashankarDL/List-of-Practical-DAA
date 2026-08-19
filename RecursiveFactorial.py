ca481e313892d61cd1585d9971b728f48ff95437
GUNASHANKAR D L <gunashankardl.132371@marwadiuniversity.ac.in>
2026-08-19 21:53:08 +0530

    Delete RecursiveFactorial.py

--- RecursiveFactorial.py
+++
@@ -1,9 +0,0 @@
-def recursive_factorial(n):
-    if n == 0 or n == 1:
-        return 1
-
-    return n * recursive_factorial(n - 1)
-arr = [5, 3, 8, 4, 2]
-n = 3
-result = recursive_factorial(n)
-print(f"Factorial of {n} is: {result}")
